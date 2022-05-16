228749a6b2c2a37a2e92f3365e9aa2b2896ec51d
JashS1008 <a1848692@adelaide.edu.au>
2022-05-16 05:50:19 +0000

    NOt completed

---
+++ eg.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+int main()
+{
+    //void read_in_Striker(string Striker.txt){
+    ifstream S_File("Striker.txt");
+    S_File.open("Striker.txt");
+    for(int i=0;i<8;i++)
+    {
+        string S_name;
+        double Finishing;
+        double shot_power;
+        double Penalty;
+        int S_Cost;
+        S_File >> S_name >> Finishing >> shot_power >> Penalty >> S_Cost;
+        cout <<  S_name << Finishing << shot_power << Penalty << S_Cost << endl;
+    }
+}
